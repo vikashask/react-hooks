74494adcda06da02c2dabfa580006d3e37d468b4
vikash <vikashraj144@gmail.com>
2020-05-02 17:42:35 +0530

    added ThemeContextProvider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, Component } from "react";
+
+export const ThemeContex = createContext();
+
+class ThemeContextProvider extends Component {
+  state = {
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+  };
+  render() {
+    return (
+      <ThemeContex.Provider value={{ ...this.state }}></ThemeContex.Provider>
+    );
+  }
+}
+
+export default ThemeContextProvider;
