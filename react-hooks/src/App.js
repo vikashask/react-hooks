85a91bb784e92fdd588ea89b594c0ca2762fe46c
vikash <vikashraj144@gmail.com>
2020-05-02 22:13:59 +0530

    react hook

---
+++ react-hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SongList from "./components/SongList";
+
+function App() {
+  return (
+    <div className="App">
+      <SongList></SongList>
+    </div>
+  );
+}
+
+export default App;
