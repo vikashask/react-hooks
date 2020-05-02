e756c0a89209085a56d6a867deb86af5e7aee6fb
vikash <vikashraj144@gmail.com>
2020-05-02 18:23:44 +0530

    toggle theme

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,9 +8,16 @@ class ThemeContextProvider extends Component {
     light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
     dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
   };
+
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  };
+
   render() {
     return (
-      <ThemeContex.Provider value={{ ...this.state }}>
+      <ThemeContex.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContex.Provider>
     );
