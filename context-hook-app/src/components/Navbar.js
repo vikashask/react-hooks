12ba79a6d2085cddcbed76b05e1e0b054a4443d2
vikash <vikashraj144@gmail.com>
2020-05-05 20:33:06 +0530

    nav bar added

--- context-hook-app/src/components/Navbar.js
+++ context-hook-app/src/components/Navbar.js
@@ -1,21 +1,24 @@
-import React, {useContext } from "react";
+import React, { useContext } from "react";
 import { ThemeContex } from "../contexts/ThemeContext";
 import { AuthContext } from "../contexts/AuthContext";
 
 const NavBar = () => {
-  const { isLightTheme, light, dark } =  useContext(ThemeContex);
-  const { isAuthenticated,toggleAuth } =  useContext(AuthContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContex);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
   const theme = isLightTheme ? light : dark;
-  return ( 
+  return (
     <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>React App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-   );
-}
- 
+      <h1>React App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? "Logged in" : "Logged out"}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
 export default NavBar;
