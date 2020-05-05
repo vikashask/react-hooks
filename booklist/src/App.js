ec59fd82a80a2258e9d42a56b9b7c539da8063ab
vikash <vikashraj144@gmail.com>
2020-05-05 20:32:36 +0530

    app updated

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import "./App.css";
 import BookContextProvider from "./contexts/BookContext";
 import NavBar from "./components/Navbar";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <NavBar />
+        <BookList />
+        <BookForm />
       </BookContextProvider>
     </div>
   );
