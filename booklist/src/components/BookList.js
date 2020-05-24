8bd2904a38abf5fd438c3240263e906addb4e25c
vikash <vikashraj144@gmail.com>
2020-05-24 19:48:59 +0530

    book list added

---
+++ booklist/src/components/BookList.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+import BookDetails from "../components/BookDetails";
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+
+  return books.length ? (
+    <div className="book-list">
+      <ul>
+        {books.map(book => {
+          return <BookDetails book={book} key={book.id} />;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty"> no book to read </div>
+  );
+};
+
+export default BookList;
